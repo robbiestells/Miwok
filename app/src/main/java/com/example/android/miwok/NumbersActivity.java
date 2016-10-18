2d561da9ff135e4d83e5f9764c8fdfaea916d5c7
Rob <robbiestells@hotmail.com>
2016-10-18 16:46:01 -0400

    more updates

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,8 +3,13 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+
+import static android.R.attr.id;
+import static android.R.attr.x;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -26,7 +31,26 @@ public class NumbersActivity extends AppCompatActivity {
         englishNumbers.add("nine");
         englishNumbers.add("ten");
 
-        Log.v("NumbersActivity", "The number at index 9 is :" + englishNumbers.get(9));
+        LinearLayout rootView =  (LinearLayout) findViewById(R.id.activity_numbers);
+//        TextView wordView = new TextView(this);
+//        wordView.setText(englishNumbers.get(0));
+//        rootView.addView(wordView);
+//
+//        TextView wordView2 = new TextView(this);
+//        wordView2.setText(englishNumbers.get(1));
+//        rootView.addView(wordView2);
+//
+//        TextView wordView3 = new TextView(this);
+//        wordView3.setText(englishNumbers.get(2));
+//        rootView.addView(wordView3);
 
+        int x = 0;
+
+        while (x < englishNumbers.size()){
+            TextView wordView = new TextView(this);
+            wordView.setText(englishNumbers.get(x));
+            rootView.addView(wordView);
+            x ++;
+        }
     }
 }
