34d8be159df143ba2fe4e5e43b0636b42a1039a5
Rob Steller <robbiestells@hotmail.com>
2016-10-18 14:58:44 -0400

    arraylist added to numbers

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> englishNumbers = new ArrayList<String>();
+
+        englishNumbers.add("one");
+        englishNumbers.add("two");
+        englishNumbers.add("three");
+        englishNumbers.add("four");
+        englishNumbers.add("five");
+        englishNumbers.add("six");
+        englishNumbers.add("seven");
+        englishNumbers.add("eight");
+        englishNumbers.add("nine");
+        englishNumbers.add("ten");
+
+        Log.v("NumbersActivity", "The number at index 9 is :" + englishNumbers.get(9));
+
     }
 }
